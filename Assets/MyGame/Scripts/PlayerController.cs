fdd4f31b249fd732ae3285070cd98bb1454cf781
NotMyTschopp <45820353+NotMyTschopp@users.noreply.github.com>
2019-12-17 13:35:02 +0100

    Clean-up PlayerController.cs
    Delete empty lines, move variables on top, delete debug messages;

--- Assets/MyGame/Scripts/PlayerController.cs
+++ Assets/MyGame/Scripts/PlayerController.cs
@@ -1,24 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    bool grounded;
+    bool gameOver = false;
+
     Rigidbody2D rb;
     Animator anim, anim2, anim3, anim4, anim5;
+
     [SerializeField] float jumpForce;
-    
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
     }
 
     // Update is called once per frame
@@ -30,15 +25,8 @@ public class PlayerController : MonoBehaviour
             {
                 jump();
             }
-
-
-
-
         }
     }
-
-    bool grounded;
-    bool gameOver = false;
 
     void jump()
     {
@@ -49,7 +37,6 @@ public class PlayerController : MonoBehaviour
         anim.SetTrigger("Jump");
 
         GameManager.instance.IncrementScore();
-        Debug.Log("DeleteMe");
     }
 
     private bool SetGameOverTrue()
@@ -57,23 +44,22 @@ public class PlayerController : MonoBehaviour
         return true;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)   {
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
         if(collision.gameObject.tag == "Ground")
         {
-            grounded = true;}
+            grounded = true;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Obstacle"){
+        if(collision.gameObject.tag == "Obstacle")
+        {
             GameManager.instance.GameOver();
             Destroy(collision.gameObject);
             anim.Play("SantaDeath");
             gameOver = SetGameOverTrue();
         }
     }
-
-
-
-
 }
