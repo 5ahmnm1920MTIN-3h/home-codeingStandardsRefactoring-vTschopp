a76132e108df38ab006a421e6102baf458572384
NotMyTschopp <45820353+NotMyTschopp@users.noreply.github.com>
2019-12-17 14:05:46 +0100

    Clean-up MenuController.cs
    Delete empty lines, delete unused usings;

--- Assets/MyGame/Scripts/MenuController.cs
+++ Assets/MyGame/Scripts/MenuController.cs
@@ -1,10 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour{
-    public void Play(){
+    public void Play()
+    {
         SceneManager.LoadScene("MainScene");
     }
 
