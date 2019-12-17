41961463237118b2fe934c5d97e1e906f3d960b8
NotMyTschopp <45820353+NotMyTschopp@users.noreply.github.com>
2019-12-17 14:09:19 +0100

    Clean-up GameManager.cs
    Delete empty lines, unused usings and default value;

--- Assets/MyGame/Scripts/GameManager.cs
+++ Assets/MyGame/Scripts/GameManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -10,8 +8,9 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     public GameObject gameOverPanel;
     public Text scoreText;
+
     string defaultText;
-    int score = 0;
+    int score;
 
     private void Awake()
     {
@@ -19,18 +18,6 @@ public class GameManager : MonoBehaviour
         {
             instance = this;
         }
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("in Start");   
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log("in Update");
     }
 
     public void GameOver()
